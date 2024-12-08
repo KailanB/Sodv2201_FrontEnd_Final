72e7475a64454b9d4515eb100a5208c2d735d94f
KailanB <kailanbates@gmail.com>
2024-12-07 22:15:57 -0700

    Create the axios fetch api call

--- src/routes/ProgramsPage/ProgramsPageMain.component.jsx
+++ src/routes/ProgramsPage/ProgramsPageMain.component.jsx
@@ -1,22 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import './ProgramsPage.style.css';
 import ProgramDiv from './ProgramDiv.component.jsx';
-import ProgramSearch from './ProgramSearch.component.jsx';
+// import ProgramSearch from './ProgramSearch.component.jsx';
+import axios from 'axios';
 
 
 const ProgramsPageMain = () => {
 
     const[programs, setPrograms] = useState([]);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
 
     useEffect(() => {
 
-        const programs = JSON.parse(localStorage.getItem('programs')) || [];
+        axios.get('http://localhost:5000/api/programs')
+        .then(res => {
+            setPrograms([...res.data]);
+            setLoading(false);
+        })
+        .catch((error) => {
+            setError(error.message);
+            setLoading(false);
+        })
 
-        if(programs)
-        {
-            setPrograms([...programs]);  
-        }
     }, []);
+
+
+    if(loading) return (
+
+        <div>
+            <h2>Software Development Programs:</h2>
+            <p>Programs Loading...</p>
+        </div>
+    );
+
+    if(error) return (
+
+        <div>
+            <h2>Software Development Programs:</h2>
+            <p>Error retrieving data: {error}</p>
+        </div>
+    );
 
     return (
 
