a990bd6d1d7e6720f9abd582b9df35d9cca34a9e
KailanB <kailanbates@gmail.com>
2024-12-07 22:16:23 -0700

    Added sign up fetch api post method

--- src/routes/SignUpPage/SignUpForm.component.jsx
+++ src/routes/SignUpPage/SignUpForm.component.jsx
@@ -3,29 +3,54 @@ import './SignUpPage.style.css';
 
 const SignUpForm = ({onAddUser}) => {
 
+    // when creating this form data make sure ALL value names MATCH the route when it pulls from the REQ BODY
+    // if casing or anything is different it will NOT PULL THE VALUES
+
+    // in my create student route I have const { FirstName, LastName, Email, Phone, Birthday, Program, Term, UserName, Password } = req.body;
+    // originally this form data was written like "firstName" and this was causing errors retrieving the data. 
+    // MATCH MATCH MATCH!
+    // MATCH THE ORDER, match the names
     const[formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        birthday: '',
-        department: '',
-        program: '',
-        term: '',
-        userName: '',
-        password: '',
-        status: 'Student',
-        studentId: '',
-        courses: []
+        FirstName: '',
+        LastName: '',
+        Email: '',
+        Phone: '',
+        Birthday: '',
+        Program: '',
+        Term: '',
+        UserName: '',
+        Password: '',
+        Status: 'Student',
+        StudentId: '',
+        Department: '',
+        Courses: []
     });
 
     const handleChange = (e) => {
 
-        
+        console.log('test');
         const {name, value} = e.target;
         setFormData({
             ...formData, 
             [name]: value
+        });
+    };
+
+    // https://stackoverflow.com/questions/47070997/how-to-get-key-prop-from-react-element-on-change
+    // getting ID value instead of input value from dropdown menus
+    // in this case we are getting the index (which dropdown menu item is selected)
+    // then requesting the attribute "id" for that dropdown menu selection
+    // this is then updated to the formData
+    // for instance if Diploma is selected Program : 1
+    // because the id attribute of Diploma selection is 1
+    const handleDropdownChange = (e) => {
+
+        const {name} = e.target;
+        const selectedIndex = e.target.options.selectedIndex;
+        const id = (e.target.options[selectedIndex].getAttribute('id'));
+        setFormData({
+            ...formData, 
+            [name]: id
         });
     };
 
@@ -34,27 +59,28 @@ const SignUpForm = ({onAddUser}) => {
     //reset form fields
     const handleSubmit = (e) => {
         e.preventDefault();
-
+        console.log(formData);
         // onAddUser is a method pass to this component from the parent component "SignUpPage"
         // we then pass all the data collected from the form to that method 
         // which adds the new user to an array
         onAddUser(formData);
+        
         setFormData({
-            firstName: '', 
-            lastName: '',
-            email: '',
-            phone: '',
-            birthday: '',
-            department: '',
-            program: '',
-            term: '',
-            userName: '',
-            password: '',
-            status: 'Student',
-            studentId: '',
+            FirstName: '',
+            LastName: '',
+            Email: '',
+            Phone: '',
+            Birthday: '',
+            Program: '',
+            Term: '',
+            UserName: '',
+            Password: '',
+            Status: 'Student',
+            StudentId: '',
+            Department: '',
             courses: []
         });
-        window.location.href = "/logInPage";
+        
     }
 
 
@@ -67,8 +93,8 @@ const SignUpForm = ({onAddUser}) => {
                     <label>First Name:</label>
                     <input 
                     type="text" 
-                    name="firstName"
-                    value={formData.firstName}
+                    name="FirstName"
+                    value={formData.FirstName}
                     onChange={handleChange}
                     className="standardInput" 
                     required></input>
@@ -77,8 +103,8 @@ const SignUpForm = ({onAddUser}) => {
                     <label>Last Name:</label>
                     <input 
                     type="text" 
-                    name="lastName"
-                    value={formData.lastName}
+                    name="LastName"
+                    value={formData.LastName}
                     onChange={handleChange}
                     className="standardInput" 
                     required></input>
@@ -87,8 +113,8 @@ const SignUpForm = ({onAddUser}) => {
                     <label>Email:</label>
                     <input 
                     type="email" 
-                    name="email"
-                    value={formData.email}
+                    name="Email"
+                    value={formData.Email}
                     onChange={handleChange}
                     className="standardInput" 
                     required></input>
@@ -97,8 +123,8 @@ const SignUpForm = ({onAddUser}) => {
                     <label>Phone:</label>
                     <input 
                     type="tel" 
-                    name="phone"
-                    value={formData.phone}
+                    name="Phone"
+                    value={formData.Phone}
                     onChange={handleChange}
                     className="standardInput" 
                     required></input>
@@ -107,8 +133,8 @@ const SignUpForm = ({onAddUser}) => {
                     <label>Birthday:</label>
                     <input 
                     type="date" 
-                    name="birthday"
-                    value={formData.birthday}
+                    name="Birthday"
+                    value={formData.Birthday}
                     onChange={handleChange}
                     className="standardInput" 
                     required></input>
@@ -116,8 +142,8 @@ const SignUpForm = ({onAddUser}) => {
                 <div className='labelInputDiv'>
                     <label for="department">Department:</label>
                     <select 
-                    name="department" 
-                    value={formData.department}
+                    name="Department" 
+                    value={formData.Department}
                     onChange={handleChange}
                     className="standardInput"
                     required>
@@ -130,38 +156,46 @@ const SignUpForm = ({onAddUser}) => {
                 <div className='labelInputDiv'>
                     <label>Program:</label>
                     <select 
-                    name="program" 
-                    value={formData.program}
-                    onChange={handleChange}
+                    name="Program" 
+                    // value={formData.Program}
+                    // here I created an id attribute for each dropdown menu item
+                    // that way we can refer to the key to send to the request.
+                    // in the DB Diploma = 1, Certificate = 2, and Post-Diploma = 3
+                    // we can probably populate this data via a request to the DB and then display each item in an option
+                    // however for the time being I have hard coded these values to match the DB
+
+                    // we use a different handle change and removed the value = {formData...} because we do not need it to match
+                    // the formData is now going to hold an ID value instead of the dropdown menu value
+                    onChange={handleDropdownChange}
                     className="standardInput" 
                     required>
                         <option></option>
-                        <option>Certificate</option>
-                        <option>Diploma</option>
-                        <option>Post-Diploma</option>
+                        <option id="2">Certificate</option>
+                        <option id="1">Diploma</option>
+                        <option id="3">Post-Diploma</option>
                     </select>
                 </div>
                 <div className='labelInputDiv'>
                     <label for="term">Term:</label>
                     <select 
-                    name="term" 
-                    value={formData.term}
-                    onChange={handleChange}
+                    name="Term" 
+                    // value={formData.Term}
+                    onChange={handleDropdownChange}
                     className="standardInput"
                     required>
                         <option></option>
-                        <option>Fall</option>
-                        <option>Winter</option>
-                        <option>Spring</option>
-                        <option>Summer</option>
+                        <option id="1">Fall</option>
+                        <option id="2">Winter</option>
+                        <option id="3">Spring</option>
+                        <option id="4">Summer</option>
                     </select>
                 </div>
                 <div className='labelInputDiv'>
                     <label>User Name:</label>
                     <input 
                     type="text" 
-                    name="userName" 
-                    value={formData.userName}
+                    name="UserName" 
+                    value={formData.UserName}
                     onChange={handleChange}
                     className="standardInput" 
                     required></input>
@@ -170,8 +204,8 @@ const SignUpForm = ({onAddUser}) => {
                     <label>Password:</label>
                     <input 
                     type="text" 
-                    name="password" 
-                    value={formData.password}
+                    name="Password" 
+                    value={formData.Password}
                     onChange={handleChange}
                     className="standardInput" 
                     required></input>
--- src/routes/SignUpPage/SignUpPage.component.jsx
+++ src/routes/SignUpPage/SignUpPage.component.jsx
@@ -2,54 +2,82 @@ import React, { useState, useEffect } from 'react';
 
 import './SignUpPage.style.css';
 import SignUpForm from './SignUpForm.component.jsx';
-
+import axios from 'axios';
 
 const SignUpPage = () => {
 
     const[users, setUsers] = useState([]);
 
+    const [response, setResponse] = useState(null);
+
+    // *******************************************************************
+    // In this case I removed almost ALL OF THE OLD CODE
+    // the old code did the email validation etc.
+    // this is no longer necessary as our middleware handles those issues now
+    // *******************************************************************
+
+
     // for using local storage https://www.freecodecamp.org/news/how-to-use-localstorage-with-react-hooks-to-set-and-get-items/
     // needed much more info on this as nothing was working
     // https://stackoverflow.com/questions/75855282/why-does-the-local-storage-get-deleted-when-i-refresh
-    useEffect(() => {
-        // this effect runs on first load of the page
-        // pulls data from local storage
-        const savedUsers = JSON.parse(localStorage.getItem('users')) || [];
-        // checks if data exists. If so, then set our users with local storage data
-        if(savedUsers)
-        {
-            setUsers([...savedUsers]);
-        }
-    }, []);
+    // useEffect(() => {
+    //     // this effect runs on first load of the page
+    //     // pulls data from local storage
+    //     const savedUsers = JSON.parse(localStorage.getItem('users')) || [];
+    //     // checks if data exists. If so, then set our users with local storage data
+    //     if(savedUsers)
+    //     {
+    //         setUsers([...savedUsers]);
+    //     }
+    // }, []);
 
     // this effect runs every time users state is updated aka this runs when we add a new user through addUser function
-    useEffect(() => {
+    // useEffect(() => {
 
-        // first check that the users array actually contains data. If it does NOT we do not want to save nothing to local storage
-        // this is a fail safe in case anything loads wonky, and the first effect runs after this one.
-        if(users.length > 0)
-        {
-            localStorage.setItem('users', JSON.stringify(users));
-        }   
-    }, [users]);
+    //     // first check that the users array actually contains data. If it does NOT we do not want to save nothing to local storage
+    //     // this is a fail safe in case anything loads wonky, and the first effect runs after this one.
+    //     if(users.length > 0)
+    //     {
+    //         localStorage.setItem('users', JSON.stringify(users));
+    //     }   
+    // }, [users]);
 
+
+    // *******************************************************************
+    // NOW all we need is a fetch request to post the formdata
+    // the biggest issue I had was matching the formData names and formData order of items
+    // also getting the ID for the post request instead of the string value name such as "Diploma"
+    // *******************************************************************
     const addUser = (user) => {
 
-        // searches the local storage for the email that is attempting to be registered
-        let userExists = users.find(savedUser => savedUser.email.toLowerCase() === user.email.toLowerCase())
-        // if no such user exists we can go ahead and add the user to our storage
-        if(!userExists)
-        {
-            // create new unqiue studentId for new user
-            user.studentId = Date.now();
-            user.email.toLowerCase();
-            setUsers((currentUserSet => [...currentUserSet, user]));
-        }
-        // else notify the user and DO not add a duplicate email.
-        else
-        {
-            alert("Sorry. Email already in use!")
-        }
+        axios.post('http://localhost:5000/api/students', user)
+        .then(res => {
+            setResponse(res.data);
+            window.location.href = "/logInPage";
+
+        })
+        .catch(error => console.error(error));
+
+
+
+
+
+
+        // // searches the local storage for the email that is attempting to be registered
+        // let userExists = users.find(savedUser => savedUser.email.toLowerCase() === user.email.toLowerCase())
+        // // if no such user exists we can go ahead and add the user to our storage
+        // if(!userExists)
+        // {
+        //     // create new unqiue studentId for new user
+        //     user.studentId = Date.now();
+        //     user.email.toLowerCase();
+        //     setUsers((currentUserSet => [...currentUserSet, user]));
+        // }
+        // // else notify the user and DO not add a duplicate email.
+        // else
+        // {
+        //     alert("Sorry. Email already in use!")
+        // }
       
     };
 
