9aedde7675370d66b871e9564b0ed1d4eaf965d6
KailanB <kailanbates@gmail.com>
2024-12-09 19:55:31 -0700

    Profile page fetches and displays properly for student / admin

--- src/routes/Profile/ProfileDiv.component.jsx
+++ src/routes/Profile/ProfileDiv.component.jsx
@@ -1,51 +1,59 @@
+import {React, useState, useEffect } from 'react';
 import './ProfilePage.style.css';
 import { GetCookieByName } from '../../Utilities.js';
 
 const ProfileDiv = (props) => {
 
-    const {firstName, lastName, id, email, phone, birthday, department, program, userName} = props
+    const {FirstName, LastName, Id, Email, PhoneNumber, Birthday, Department, Program, UserName} = props
+
+    const[role, setRole] = useState("");
+    useEffect(() => {
+        let userRole = GetCookieByName("role=");
+        setRole(userRole);
+
+    }, []);
 
 
 
-    let userEmail = GetCookieByName("userEmail=");
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    let currentUser;
+    // let userEmail = GetCookieByName("userEmail=");
+    // const users = JSON.parse(localStorage.getItem('users')) || [];
+    // let currentUser;
     // if user has a cookie check if user is admin or student
     // then return appropriate nav bar
-    if(userEmail)
+    if(role)
     {   
 
-        currentUser = users.find(user => user.email.toLowerCase() === userEmail.toLowerCase());
-        if(currentUser.status == "Admin")
+        //currentUser = users.find(user => user.email.toLowerCase() === userEmail.toLowerCase());
+        if(role == "admin")
         {
             return (
                 
                 <div className="profileDiv">
-                    <h2>{firstName} {lastName}</h2>
-                    <p>Birthday: {birthday}</p>
-                    <p>Username: {userName}</p>
+                    <h2>{FirstName} {LastName}</h2>
+                    <p>Birthday: {Birthday}</p>
+                    <p>Username: {UserName}</p>
                     <br />
                     <p><strong>Contact information: </strong></p>
-                    <p>Email: {email}</p>
-                    <p>Phone: {phone}</p>
+                    <p>Email: {Email}</p>
+                    <p>Phone: {PhoneNumber}</p>
                 </div>
             );
         }
-        else if (currentUser.status == "Student")
+        else if (role == "student")
         {
             return (
 
                 <div className="profileDiv">
-                    <h2>{firstName} {lastName}</h2>
-                    <p>StudentID: {id}</p>
-                    <p>Birthday: {birthday}</p>
-                    <p>Department: {department}</p>
-                    <p>Program: {program}</p>
-                    <p>Username: {userName}</p>
+                    <h2>{FirstName} {LastName}</h2>
+                    <p>StudentID: {Id}</p>
+                    <p>Birthday: {Birthday}</p>
+                    <p>Department: {Department}</p>
+                    <p>Program: {Program}</p>
+                    <p>Username: {UserName}</p>
                     <br />
                     <p><strong>Contact information: </strong></p>
-                    <p>Email: {email}</p>
-                    <p>Phone: {phone}</p>
+                    <p>Email: {Email}</p>
+                    <p>Phone: {PhoneNumber}</p>
                 </div>
             );
         }
--- src/routes/Profile/ProfilePage.component.jsx
+++ src/routes/Profile/ProfilePage.component.jsx
@@ -2,27 +2,93 @@ import React, { useState, useEffect } from 'react';
 
 import ProfileDiv from './ProfileDiv.component.jsx';
 import { GetCookieByName } from '../../Utilities.js';
+import axios from 'axios';
 
 function ProfilePage() {
    
     const[user, setUser] = useState([]);
 
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+
+    const[role, setRole] = useState("");
+
+
     useEffect(() => {
+        let userRole = GetCookieByName("role=");
+        setRole(userRole);
+    }, []);
+  
+    useEffect(() => {
+  
+        
+        if(role === 'admin')
+        {
+            axios.get('http://localhost:5000/api/admin/byId', {withCredentials: true})
+            .then(res => {
+                
+                setUser(res.data);
+                console.log(user);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+        });
+        }
+        else if(role === 'student')
+        {
+            axios.get('http://localhost:5000/api/students/byId', {withCredentials: true})
+            .then(res => {
+                setUser(res.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+            });
+        }
+
+    }, [role]);
+
+
+
+
+    // useEffect(() => {
 
         
-        let userEmail = GetCookieByName("userEmail=")
-        const savedUsers = JSON.parse(localStorage.getItem('users')) || [];       
-        let userExists = savedUsers.find(savedUser => savedUser.email.toLowerCase() === userEmail.toLowerCase());
+    //     let userEmail = GetCookieByName("userEmail=")
+    //     const savedUsers = JSON.parse(localStorage.getItem('users')) || [];       
+    //     let userExists = savedUsers.find(savedUser => savedUser.email.toLowerCase() === userEmail.toLowerCase());
 
-        if(userExists)
-        {
-            setUser(userExists);  
-        }
-    }, []);  
+    //     if(userExists)
+    //     {
+    //         setUser(userExists);  
+    //     }
+    // }, []);  
+
+
+
+    if(loading)
+    {
+        return (
+            <div>
+                <p>Loading data...</p>
+            </div>
+        );
+    }
+    if(error)
+    {
+        return (
+            <div>
+                <p>Error loading data ...{error}</p>
+            </div>
+        );
+    }
 
     return (
         <div>
-            <ProfileDiv firstName={user.firstName} lastName={user.lastName} id={user.studentId} email={user.email} phone={user.phone} birthday={user.birthday} department={user.department} program={user.program} userName={user.userName}/>
+            <ProfileDiv FirstName={user.FirstName} LastName={user.LastName} Id={user.StudentID} Email={user.Email} PhoneNumber={user.PhoneNumber} Birthday={user.Birthday} Department={user.Department} Program={user.Program} UserName={user.UserName}/>
         </div>
     );
 }
