52aa10b9387d1d6332d62ef476cd64bd60b58c2e
kajal-958 <163466004+kajal-958@users.noreply.github.com>
2024-12-11 01:53:20 -0700

    update logout with axios

--- src/routes/LogOut/LogoutPage.component.jsx
+++ src/routes/LogOut/LogoutPage.component.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import './LogOutPage.style.css';
 
-// I created this page because we have used token for our backend, and this file was needed to actually logout on browser.
 const LogOutPage = () => {
   const handleLogout = async () => {
     try {
-      const response = await fetch('/api/auth/logout', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-        },
-      });
+      
+      const response = await axios.post('http://localhost:5000/api/auth/logout', {}, { withCredentials: true });
 
-      if (response.ok) {
-        // Clear token and redirect to login
+      if (response.status === 200) {
+        // Clear token
         localStorage.removeItem('authToken');
         alert('Logged out successfully!');
         window.location.href = '/logInPage';
       } else {
-        const data = await response.json();
-        alert(data.error || 'Logout failed');
+        alert('Logout failed');
       }
     } catch (error) {
       console.error('Error logging out:', error);
