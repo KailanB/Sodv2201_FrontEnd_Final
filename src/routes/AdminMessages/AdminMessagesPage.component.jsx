898c9056e6276178824f4e9bb1ed1dd1b04020ad
KailanB <kailanbates@gmail.com>
2024-12-10 20:02:06 -0700

    Updated admin retrieve messages to axios fetch

--- src/routes/AdminMessages/AdminMessagesPage.component.jsx
+++ src/routes/AdminMessages/AdminMessagesPage.component.jsx
@@ -1,14 +1,50 @@
 import {React, useState, useEffect} from 'react';
 import './MessagePage.style.css';
 import MessageDiv from './MessageDiv.component.jsx';
-
+import axios from 'axios';
 
 
 const AdminMessagesPage = () => {
 
 
-    const [messages, setMessages]= useState(() => JSON.parse(localStorage.getItem('messages')) || []);
+    // const [messages, setMessages]= useState(() => JSON.parse(localStorage.getItem('messages')) || []);
 
+    const [messages, setMessages] = useState([]);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+
+    useEffect(() => {
+
+        
+        axios.get('http://localhost:5000/api/admin/messages', {withCredentials: true})
+        .then(res => {
+            setMessages([...res.data]);
+            setLoading(false);
+        })
+        .catch((error) => {
+            setError(error.message);
+            setLoading(false);
+        });
+
+    }, []);
+
+    if(loading)
+    {
+        return (
+            <div>
+                <p>Loading student data...</p>
+            </div>
+        );
+    }
+    if(error)
+    {
+        return (
+            <div>
+                <p>Error loading data ...{error}</p>
+            </div>
+        );
+    }
+      
 
     return (
 
@@ -16,7 +52,7 @@ const AdminMessagesPage = () => {
             <h2>INFOMATION REQUESTS</h2><br></br>
             {messages.map((message, index) => (
                 <div key={index}>
-                    <MessageDiv fullName={message.fullName} email={message.email} message={message.message}/>
+                    <MessageDiv FullName={message.FullName} Email={message.Email} Message={message.Message}/>
                     <br />
                 </div>
                 
